6c28a9d8daf3887becfa209839b9f3f521a4e6a8
Tetyana <tetyana.chouteau@gmail.com>
2023-03-05 09:18:28 +0100

    Accessibilitée validé par WAVE

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -9,6 +9,7 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute("alt", "Photo de " + name);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         article.appendChild(img);
