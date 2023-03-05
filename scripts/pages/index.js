418bed1724b627c5352745cb37c5b311c75fe3d6
Tetyana <tetyana.chouteau@gmail.com>
2023-03-05 09:05:09 +0100

    Récupération du json avec fetch Ajout des photos manquantes

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,30 +2,20 @@
         console.log("initialisation des données");
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        let photographers = []; 
+        
+        await fetch('data/photographers.json')
+        .then(function(response) {
+            console.log(response)
+            return response.json();
+        })
+        .then(function(json) {
+            photographers = json;
+        });
         // et bien retourner le tableau photographers seulement une fois récupéré
-        console.log("data: \n" + JSON.stringify(photographers));
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        console.log("data en json: \n" + photographers);
+        console.log("data en text: \n" + JSON.stringify(photographers));
+        return photographers;
     }
 
     async function displayData(photographers) {
