5faec33e87eb01b5a4f258d8ad306df80ad1abd3
Tetyana <tetyana.chouteau@gmail.com>
2023-03-05 09:37:02 +0100

    Style du texte sous profile Ajoute d'aria-label

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -12,8 +12,21 @@ function photographerFactory(data) {
         img.setAttribute("alt", "Photo de " + name);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        h2.setAttribute('aria-label', 'nom du photographe')
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = city + ", " + country;
+        h2.setAttribute('aria-label', 'adresse du photographe')
+        const h4 = document.createElement( 'h4' );
+        h4.textContent = tagline;
+        h2.setAttribute('aria-label', 'citation du photographe')
+        const h5 = document.createElement( 'h5' );
+        h5.textContent = price + "€/jour";
+        h2.setAttribute('aria-label', 'tarif journalier du photographe')
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(h5);
         return (article);
     }
     return { name, picture, getUserCardDOM }
