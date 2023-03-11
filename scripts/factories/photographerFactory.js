f0c4eb26bd1585965179daf87d40ac154f404bc6
Tetyana <tetyana.chouteau@gmail.com>
2023-03-11 14:43:50 +0100

    Création fonction recupération données photographe

--- scripts/factories/photographerFactory.js
+++ scripts/factories/photographerFactory.js
@@ -1,25 +1,25 @@
 function photographerFactory(data) {
     console.log("entrée dans la factory");
-    const { name, id, city, country, tagline, price,  portrait  } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         console.log("création de l'objet html dom");
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
         img.setAttribute("alt", "Photo de " + name);
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
         h2.setAttribute('aria-label', 'nom du photographe')
-        const h3 = document.createElement( 'h3' );
+        const h3 = document.createElement('h3');
         h3.textContent = city + ", " + country;
         h2.setAttribute('aria-label', 'adresse du photographe')
-        const h4 = document.createElement( 'h4' );
+        const h4 = document.createElement('h4');
         h4.textContent = tagline;
         h2.setAttribute('aria-label', 'citation du photographe')
-        const h5 = document.createElement( 'h5' );
+        const h5 = document.createElement('h5');
         h5.textContent = price + "€/jour";
         h2.setAttribute('aria-label', 'tarif journalier du photographe')
         article.appendChild(img);
@@ -29,20 +29,19 @@ function photographerFactory(data) {
         article.appendChild(h5);
         // ajout evemenent click qui appelle la fonction qui change de page
         // on passe l'id du photographe
-        article.addEventListener("click",() => { linkPhotographer(id) }); 
+        article.addEventListener("click", () => { linkPhotographer(id) });
         return (article);
     }
 
     //page phot. html, das factory.js
-    profilCardDOM(){
-        const { name,  portrait, city, tagline,country }= data;
+    function profilCardDOM(){
+        const { name, portrait, city, tagline, country } = data;
         //2 interpolation 
         return `
-        <article><h2>${city}</aricle>;
-
+        <article><h2>${city}</aricle>
         `
-     }
-    return { name, picture, getUserCardDOM }
+    }
+    return { name, picture, getUserCardDOM, profilCardDOM }
 
 
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,29 @@
+async function createProfilCardDom() {
+    // récupère l'id passé en paramètre
+    let location = document.location;
+    let url = new URL(location);
+    let params = url.searchParams;
+    const id = params.get("id")
+    // si l'id existe dans l'url
+    if (id) {
+        // récupère les données photographe
+        await fetch('data/photographers.json')
+        .then(function(response) {
+            console.log(response)
+            return response.json();
+        })
+        .then(function(json) {
+            // on filtre le tableau des photographes pour récupérer celui qui à l'id de la page
+            // ensuite on recupère le permier element trouvé (normalement le seul)
+            photographers = json.photographers.filter(el => { el.id === id})[0];
+        });
+    } else {
+        // si pas d'id on retourne sur la page d'acceuil
+        document.location = "index.html";
+    }
+}
 //Mettre le code JavaScript lié à la page photographer.html
+
+createProfilCardDom()
+
+ //faire module et mporter fFactory from ...
